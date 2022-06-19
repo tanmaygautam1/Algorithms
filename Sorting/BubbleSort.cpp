d095a341671bebd46a90e7faf922c4942c1388c1
tanmaygautam1 <70182169+tanmaygautam1@users.noreply.github.com>
2022-06-19 14:35:34 +0530

    Update BubbleSort.cpp

--- Sorting/BubbleSort.cpp
+++ Sorting/BubbleSort.cpp
@@ -1,30 +1,29 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void BubbleSort(int arr[], int n){
-  int i,j;
- for(i=0;i<n;i++){
-    for(j=i+1;j<n;j++){
-      if(arr[i]>arr[j]){
-        
-        int temp = arr[i];
-        arr[i]=arr[j];
-        arr[j]=temp;
-      }
-    }
- }
- for(int f=0; f<n;f++){
-  cout<<arr[f]<<endl;
- }
+ 
+void bubbleSort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
 }
-int main(){  
-  int x;
-cout<<"Enter the size of array:";
-cin>>x;
-
-  int arrayy[x]={0};
-  for(int i=0;i<x;i++){
-    cout<<"Enter Element "<<i+1<<":";
-    cin>>arrayy[i];
-  }
-  BubbleSort(arrayy,x);
+ 
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+ 
+int main()
+{
+    int arr[] = { 5, 1, 4, 2, 8};
+    int N = sizeof(arr) / sizeof(arr[0]);
+    bubbleSort(arr, N);
+    cout << "Sorted array: \n";
+    printArray(arr, N);
+    return 0;
 }
